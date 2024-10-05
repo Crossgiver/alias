0e38fbfce6250e09c9263fae50453801ce62017e
Harut_Khachatryan <harut_khachatryan@epam.com>
2024-10-05 14:58:52 +0400

    fix html path

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/alias'));
+app.use(express.static(__dirname + '/dist/alias/browser'));
 
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/alias/index.html'));});
+    '/dist/alias/browser/index.html'));});
 
 app.listen(process.env.PORT || 8080);
